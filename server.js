f1a71bea0f54c4267d1e6aa64ccd743a3040b69d
Ashish Gaikwad <ash.gkwd@gmail.com>
2015-12-11 22:28:05 +0530

    TEST - index.html added to test socket.io

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 // respond with hello world when GET request is made
 app.get('/', function(req, res) {
-	res.send('Hello World');
+	res.sendFile(__dirname + '/index.html');
+});
+
+// io connection response
+io.on('connection', function(socket) {
+	console.log('a user connected');
 });
 
 // Listen on port 3001
-var server = app.listen(3001, function() {
-	console.log('Listening on : ' + server.address().port);
-})
+var server = http.listen(3001, function() {
+	console.log('Listening on : http://localhost:' + server.address().port);
+});
