705cc97053dacae5e2f1d41c4f9d3315b1cb96be
Ashish Gaikwad <ash.gkwd@gmail.com>
2015-12-14 21:02:26 +0530

    IMPROVED - SQL file for creating tables PostgresSQL

---
+++ setup/init_db.sql
@@ -0,0 +1,12 @@
+-- Drop Table marker;
+-- Drop Table author;
+-- Drop Table popup;
+
+-- Table : "marker"
+Create Table marker(id bigint primary key, lat real, lng real, author bigint, popup uuid);
+
+-- Table : "author"
+Create Table author(id bigint primary key, name varchar(80), twitter varchar(20), home_city varchar(40));
+
+-- Table : "popup"
+Create Table popup(id uuid primary key, post_text varchar(255), hash_tags varchar(255), time timestamp);
