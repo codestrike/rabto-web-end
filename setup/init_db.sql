69bc12a22d84fff109f20c72ce4af4c7c5c27a4a
Ashish Gaikwad <ash.gkwd@gmail.com>
2015-12-17 08:54:58 +0530

    FIX - Primary Key of popup table is bigint (serial) instead of uuid

--- setup/init_db.sql
+++ setup/init_db.sql
@@ -3,10 +3,10 @@
 -- Drop Table popup;
 
 -- Table : "marker"
-Create Table marker(id serial primary key, lat real, lng real, author bigint, popup uuid);
+Create Table marker(id serial primary key, lat real, lng real, author bigint, popup bigint);
 
 -- Table : "author"
 Create Table author(id serial primary key, name varchar(80), twitter varchar(20), home_city varchar(40));
 
 -- Table : "popup"
-Create Table popup(id uuid primary key, post_text varchar(255), hash_tags varchar(255), time timestamp);
+Create Table popup(id serial primary key, post_text varchar(255), hash_tags varchar(255), time timestamp);
