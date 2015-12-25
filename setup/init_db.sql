4e39e7373355f5652e5d5df45a3d015cd7c233b2
Ashish Gaikwad <ash.gkwd@gmail.com>
2015-12-26 03:10:48 +0530

    IMPROVED - Added column in popup table to store URL of image

--- setup/init_db.sql
+++ setup/init_db.sql
@@ -9,4 +9,4 @@ Create Table marker(id serial primary key, lat real, lng real, author bigint);
 Create Table author(id serial primary key, name varchar(80), twitter varchar(20), home_city varchar(40));
 
 -- Table : "popup"
-Create Table popup(id serial primary key, post_text varchar(255), hash_tags varchar(255), time timestamp, marker bigint);
+Create Table popup(id serial primary key, post_text varchar(255), post_image varchar(120), hash_tags varchar(255), time timestamp, marker bigint);
